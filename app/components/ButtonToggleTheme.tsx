0170da8494256376e4eac75ca2c6688033b114e0
Maxim <banagamaxim13@gmail.com>
2025-04-19 22:14:38 +0300

    Tweaked the NavBar

--- app/components/ButtonToggleTheme.tsx
+++ app/components/ButtonToggleTheme.tsx
@@ -21,14 +21,47 @@ const ThemeToggle = () => {
 
   return (
     <button
-      className="absolute right-2 cursor-pointer transition-all duration-150 ease-in-out
+      className="absolute right-2 flex flex-row cursor-pointer transition-all duration-150 ease-in-out
        bg-elements-light dark:bg-elements-dark rounded-lg border-2 border-border-light dark:border-border-dark 
-       hover:bg-interactive/50 hover:scale-110 active:scale-90"
+       hover:bg-interactive-light dark:hover:bg-interactive-dark hover:scale-110 active:scale-90"
       onClick={handleThemeToggle}
     >
-      {isDark ? <CiLight size="2em" color="#F9F9F9" /> : <CiDark size="2em" />}
+      {!isDark ? (
+        <CiLight size="2em" className="dark:[visibility:hidden]" />
+      ) : (
+        <CiDark
+          size="2em"
+          className="[visibility:hidden] dark:visible"
+          color="#F9F9F9"
+        />
+      )}
     </button>
   );
 };
 
 export default ThemeToggle;
+
+{
+  /* */
+}
+
+{
+  /* <button
+      className="absolute right-2 flex flex-row cursor-pointer transition-all duration-150 ease-in-out
+       bg-elements-light dark:bg-elements-dark rounded-lg border-2 border-border-light dark:border-border-dark 
+       hover:bg-interactive/50 hover:scale-110 active:scale-90"
+      onClick={handleThemeToggle}
+    >
+      {isDark ? (
+        <span className="flex flex-row">
+          <CiLight size="1.5em" className="" />
+          <CiDark size="1.5em" color="#F9F9F9" />
+        </span>
+      ) : (
+        <span className="flex flex-row">
+          <CiLight size="1.5em" />
+          <CiDark size="1.5em" color="#F9F9F9" />
+        </span>
+      )}
+    </button> */
+}
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,7 +5,10 @@ import ThemeToggle from "./ButtonToggleTheme";
 const NavBar = () => {
   return (
     <div className="flex flex-row sticky justify-center items-center w-full h-fit p-2">
-      <ul className="flex gap-4 text-t-light dark:text-t-dark justify-center text-2xl w-fit">
+      <ul
+        className="flex gap-4 text-t-light dark:text-t-dark justify-center text-2xl w-fit *:hover:text-interactive-light *:dark:hover:text-interactive-light
+      *:transition-all *:duration-250 *:ease-in-out"
+      >
         <Link href="/">Home</Link>
         <Link href="/projects">Projects</Link>
         <Link href="/contacts">Contacts</Link>
