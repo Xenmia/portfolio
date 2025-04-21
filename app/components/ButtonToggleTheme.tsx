927d8be8e18d233b0a142ea8abc81a23fd850114
Maxim <banagamaxim13@gmail.com>
2025-04-21 18:35:10 +0300

    Implemented next-themes

--- app/components/ButtonToggleTheme.tsx
+++ app/components/ButtonToggleTheme.tsx
@@ -1,67 +1,29 @@
 "use client";
 import { useState, useEffect } from "react";
 import { CiLight, CiDark } from "react-icons/ci";
+import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState<boolean>(false);
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { setTheme, resolvedTheme } = useTheme();
 
-  useEffect(() => {
-    const html = document.documentElement;
-
-    if (isDark) {
-      html.classList.add("dark");
-    } else {
-      html.classList.remove("dark");
-    }
-  }, [isDark]);
-
-  const handleThemeToggle = () => {
-    setIsDark((previous) => !previous);
-  };
-
+  useEffect(() => setMounted(true), []);
+  console.log("Meow");
+  if (!mounted) return <></>;
   return (
     <button
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
       className="absolute right-2 flex flex-row cursor-pointer transition-all duration-150 ease-in-out
        bg-elements-light dark:bg-elements-dark rounded-lg border-2 border-border-light dark:border-border-dark 
        hover:bg-interactive-light dark:hover:bg-interactive-dark hover:scale-110 active:scale-90"
-      onClick={handleThemeToggle}
     >
-      {!isDark ? (
+      {resolvedTheme === "light" ? (
         <CiLight size="2em" className="dark:[visibility:hidden]" />
       ) : (
-        <CiDark
-          size="2em"
-          className="[visibility:hidden] dark:visible"
-          color="#F9F9F9"
-        />
+        <CiDark size="2em" className="visible " />
       )}
     </button>
   );
 };
 
 export default ThemeToggle;
-
-{
-  /* */
-}
-
-{
-  /* <button
-      className="absolute right-2 flex flex-row cursor-pointer transition-all duration-150 ease-in-out
-       bg-elements-light dark:bg-elements-dark rounded-lg border-2 border-border-light dark:border-border-dark 
-       hover:bg-interactive/50 hover:scale-110 active:scale-90"
-      onClick={handleThemeToggle}
-    >
-      {isDark ? (
-        <span className="flex flex-row">
-          <CiLight size="1.5em" className="" />
-          <CiDark size="1.5em" color="#F9F9F9" />
-        </span>
-      ) : (
-        <span className="flex flex-row">
-          <CiLight size="1.5em" />
-          <CiDark size="1.5em" color="#F9F9F9" />
-        </span>
-      )}
-    </button> */
-}
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,14 +4,11 @@ import ThemeToggle from "./ButtonToggleTheme";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row sticky justify-center items-center w-full h-fit p-2 top-0 bg-primary-light dark:bg-primary-dark border-2 border-border-light dark:border-border-dark">
-      <ul
-        className="flex gap-4 text-t-light dark:text-t-dark justify-center text-2xl w-fit *:hover:text-interactive-light *:dark:hover:text-interactive-light
-      *:transition-all *:duration-250 *:ease-in-out"
-      >
-        <Link href="/">Home</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/contact">Contact</Link>
+    <div className="flex flex-row sticky justify-center items-center w-full h-fit p-2 top-0 bg-primary-light dark:bg-primary-dark border-b-2 border-border-light dark:border-border-dark">
+      <ul className="flex gap-4 text-t-light dark:text-t-dark justify-center text-2xl w-fit *:hover:text-interactive-light *:dark:hover:text-interactive-light">
+        <Link href="/">HOME</Link>
+        <Link href="/projects">PROJECTS</Link>
+        <Link href="/contact">CONTACT</Link>
       </ul>
       <ThemeToggle />
     </div>
