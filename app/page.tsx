092fb897abdd8bf969d81692b9f06ea59b5dcbf6
Maxim <banagamaxim13@gmail.com>
2025-04-23 16:18:27 +0300

    Implemented the new background; Added the Hero and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import ProjectPreview from "./components/ProjectPreview";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-[100vh] w-[100vw] **:transition-all **:duration-150 **:ease-in-out bg-primary-light dark:bg-primary-dark">
+    <div
+      className="flex flex-col bg-primary-light dark:bg-primary-dark bg-[image:var(--gradient-bg-light)] 
+      dark:bg-[image:var(--gradient-bg-dark)] bg-size-[40px_40px] bg-center bg-repeat"
+    >
+      <Hero />
       <AboutMe />
+      <div className="flex flex-row">
+        <ProjectPreview />
+        <ContactMe />
+      </div>
+      <Footer />
     </div>
   );
 }
