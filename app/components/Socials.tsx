9214155f49f748a5a59c4ba2734d2e8183de84b1
Maxim <banagamaxim13@gmail.com>
2025-04-23 10:22:38 +0300

    Socials component for ContactMe.tsx

---
+++ app/components/Socials.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { TbBrandLinkedin, TbBrandGithub } from "react-icons/tb";
+import { PiTelegramLogoFill } from "react-icons/pi";
+const Socials = () => {
+  const socials = [
+    {
+      address: "https://www.linkedin.com/in/maxim-banaga-38060433b/",
+      icon: <TbBrandLinkedin className="h-full w-full" />,
+      name: "LinkedIn",
+    },
+    {
+      address: "https://github.com/Xenmia",
+      icon: <TbBrandGithub className="h-full w-full" />,
+      name: "GitHub",
+    },
+    {
+      address: "https://t.me/wxzwzy",
+      icon: <PiTelegramLogoFill className="h-full w-full" />,
+      name: "Telegram",
+    },
+  ];
+  return (
+    <div className="flex flex-row justify-evenly my-auto">
+      {socials.map((social, index) => (
+        <div key={index} className="flex gap-2 items-center">
+          <a
+            href={social.address}
+            className={
+              "flex flex-col h-auto w-16 p-2 bg-transparent items-center rounded-sm shadow-2xs hover:*:scale-105"
+            }
+          >
+            {social.icon}
+            <h1 className="scale-0 text-t-light dark:text-t-dark">
+              {social.name}
+            </h1>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Socials;
