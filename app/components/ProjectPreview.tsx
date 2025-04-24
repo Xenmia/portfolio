88211e8d7ef4d899b038524a8e5a67046a4745d5
Maxim <banagamaxim13@gmail.com>
2025-04-24 19:46:05 +0300

    Positioning tweaks

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -18,19 +18,21 @@ const inter = Inter({
 const ProjectPreview = () => {
   return (
     <div
-      className="flex flex-col flex-2/3 border-muted-light dark:border-muted-dark border-2 
+      className="flex flex-col lg:flex-row flex-2/3 border-muted-light dark:border-muted-dark border-2 
     bg-background-light dark:bg-background-dark overflow-hidden origin-left rounded-4xl shadow-2xl"
     >
-      <a
-        className="decoration-text-light dark:decoration-text-dark flex flex-col lg:flex-row"
-        href="https://boanex.netlify.app/"
-      >
+      <a href="https://boanex.netlify.app/">
         <Image
-          className="w-full lg:min-w-2/3 border-2 border-muted-light dark:border-muted-dark"
+          className="w-full lg:min-w-2/3 border-r-2 border-muted-light dark:border-muted-dark"
           alt="project preview"
           src={mockup}
         />
-        <div className="flex flex-col *:px-4 *:pt-2 *:underline-offset-4 hover:*:underline">
+      </a>
+      <div className="flex flex-col h-full p-2">
+        <a
+          href="https://boanex.netlify.app/"
+          className="*:underline-offset-4 hover:*:underline *:px-4 *:pt-2"
+        >
           <h1
             className={
               "text-4xl text-text-light dark:text-text-dark " + kanit.className
@@ -40,22 +42,22 @@ const ProjectPreview = () => {
           </h1>
           <h1
             className={
-              "text-2xl text-text-light dark:text-text-dark font-semibold " +
+              "text-2xl text-muted-light dark:text-muted-dark font-semibold " +
               inter.className
             }
           >
             Business website built with HTML/CSS under tight constraints.
           </h1>
-        </div>
-      </a>
-      <Link
-        className="text-text-light dark:text-text-dark hover:underline text-4xl 
-        lg:place-self-end my-auto place-self-center lg:px-4 pb-2 
+        </a>
+        <Link
+          className="text-text-light dark:text-text-dark hover:underline text-4xl 
+          lg:place-self-end mt-8 lg:mt-auto place-self-center lg:px-4 pb-2 
         hover:text-secondary-light dark:hover:text-secondary-dark "
-        href="../projects"
-      >
-        See more projects
-      </Link>
+          href="../projects"
+        >
+          See more projects
+        </Link>
+      </div>
     </div>
   );
 };
