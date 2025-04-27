99d1c5dadb9759c0037f189b39105aefd418edee
Maxim <banagamaxim13@gmail.com>
2025-04-27 18:53:09 +0300

    Implemented the new fixed background component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutMe from "./components/AboutMe";
+import Background from "./components/Background";
 import ContactMe from "./components/ContactMe";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
@@ -7,11 +8,8 @@ import ScrollTrack from "./components/ScrollTrack";
 
 export default function Home() {
   return (
-    <div
-      className="flex flex-col bg-background-light dark:bg-background-dark 
-      bg-[url(./pictures/pattern-light.png)] dark:bg-[url(./pictures/pattern-dark.png)] 
-      bg-center bg-repeat text-text-light dark:text-text-dark"
-    >
+    <div className="flex flex-col text-text-light dark:text-text-dark">
+      <Background />
       <Hero />
       <AboutMe />
       <div className="flex lg:flex-row flex-col my-30 mx-4 gap-8 lg:mx-16 lg:gap-16 justify-between">
