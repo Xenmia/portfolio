ceba6d262d7791517a09d5b50d964d17710df142
Maxim <banagamaxim13@gmail.com>
2025-04-27 18:52:48 +0300

    Added a fixed background component

---
+++ app/components/Background.tsx
@@ -0,0 +1,7 @@
+const Background = () => {
+  return (
+    <div className="h-screen w-screen fixed -z-50 bg-[url(./pictures/pattern-light.png)] dark:bg-[url(./pictures/pattern-dark.png)]" />
+  );
+};
+
+export default Background;
