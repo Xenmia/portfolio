ea65f7722612dd57ba2da8c916cb84cca74a7551
Maxim <banagamaxim13@gmail.com>
2025-05-01 10:55:14 +0300

    Small tweaks

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectSelect from "./components/ProjectSelect";
 
 const Projects = () => {
   return (
-    <div className="flex h-full w-screen overflow-hidden relative text-text-light dark:text-text-dark ">
+    <div className="flex h-full w-screen overflow-hidden relative">
       <Background />
 
       <ProjectSelect />
